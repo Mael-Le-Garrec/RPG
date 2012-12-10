240102b0dd9a5158af8ef140758d53766eb989b3
Hatrix <hatrix@hatrix.fr>
2012-12-10 22:26:03 +0100

    Système de TP
    Système de TP pour entrer dans les batiments.
    Actuellement, on peut se tp en avancant sur la case quelque soit la
    direction.

--- classes.py
+++ classes.py
@@ -13,6 +13,7 @@ class Carte:
         self.fond = pygame.image.load("textures\\fond.png")
         self.collisions = []
         self.bloc = []
+        self.tp = []
     
     def charger_carte(self):
         '''On charge une carte en fonction du nom donné (0, 1, etc)
@@ -36,21 +37,44 @@ class Carte:
         # self.coords[i][1][0] => composante x du second point de l'intervale
         # self.coords[i][1][1] => composante y du second point de l'intervale
         # self.coords[i][2] => texture des murs
+        # self.coords[i][3] => texture traversable ou non
+        # self.coords[i][4] => vers quelle position la zone téléporte
+        # self.coords[i][5] => vers quelle carte la zone téléporte
+        
+        ## ORIENTATION !!!
         
         # coords[i][1][0] - coords[i][0][0] = nb de repets en x
         # coords[i][1][1] - coords[i][0][1] = nb de repets en y
          
         for i in range(len(self.lignes)):
-            if re.match("^[0-9]*:[0-9]*;[0-9]*:[0-9]*;[0-9]*", self.lignes[i]):
+            if re.match("^[0-9]*:[0-9]*;[0-9]*:[0-9]*;[a-zA-Z0-9]*", self.lignes[i]):
                 self.coords.append(self.lignes[i])
                 self.coords[-1] = self.coords[-1].rstrip().split(";")
                 self.coords[-1][0] = self.coords[-1][0].split(":")
                 self.coords[-1][1] = self.coords[-1][1].split(":")
-        
+                
+                try:
+                    self.coords[-1][4] = self.coords[-1][4].split(":")
+                except:
+                    pass
+
+                    
         for i in range(len(self.coords)):
             self.textures[self.coords[i][2]] =(pygame.image.load("textures\{0}.png".format(self.coords[i][2])).convert_alpha())
+            try:
+                self.tp.append([int(self.coords[i][3]), (int(self.coords[i][0][0]) + 100, int(self.coords[i][0][1]) + 150), (int(self.coords[i][4][0]) + 100, int(self.coords[i][4][1]) + 150)])
+            except:
+                pass
         
-        # print(self.coords[1])
+        # self.tp[i][0] => map destination
+        # self.tp[i][1][0] => x map actuelle
+        # self.tp[i][1][1] => y map actuelle 
+        
+        # self.tp[i][2][0] => x map destination
+        # self.tp[i][2][1] => y map destination
+        
+        
+        # print(self.tp)
         
         for i in range(len(self.coords)):
             for j in range(0,(int(self.coords[i][1][0]) - int(self.coords[i][0][0])) // 30):
@@ -103,6 +127,7 @@ class Joueur:
             if self.perso_b == self.orientation:
                 self.orientation = self.perso_b
                 if (self.position_x, self.position_y+30) not in liste_cartes[perso.carte].collisions:
+                        
                     if self.position_y < 700:
                         self.position_y += 30
                     else:
@@ -111,6 +136,15 @@ class Joueur:
                         liste_cartes[int(liste_cartes[perso.carte].directions["bas"])].afficher_carte(fenetre)
                         perso.carte = int(liste_cartes[perso.carte].directions["bas"])
                         perso.position_y = 150
+                        
+                    # Système de TP
+                    for i in range(len(liste_cartes[perso.carte].tp)):
+                        if liste_cartes[perso.carte].tp[i][1] == (self.position_x, self.position_y):
+                            self.position_x = liste_cartes[perso.carte].tp[i][2][0]
+                            self.position_y = liste_cartes[perso.carte].tp[i][2][1]
+                            perso.carte = liste_cartes[perso.carte].tp[i][0]
+                            
+                            
             else:
                 self.orientation = self.perso_b
 
@@ -118,12 +152,21 @@ class Joueur:
             if self.perso_h == self.orientation:
                 self.orientation = self.perso_h
                 if (self.position_x, self.position_y-30) not in liste_cartes[perso.carte].collisions:
+ 
                     if self.position_y > 150:
                         self.position_y -= 30
                     else:
                         liste_cartes[int(liste_cartes[perso.carte].directions['haut'])].afficher_carte(fenetre)
                         perso.carte = int(liste_cartes[perso.carte].directions['haut'])
                         perso.position_y = 150+600-30
+                        
+                        
+                    for i in range(len(liste_cartes[perso.carte].tp)):
+                        if liste_cartes[perso.carte].tp[i][1] == (self.position_x, self.position_y):
+                            self.position_x = liste_cartes[perso.carte].tp[i][2][0]
+                            self.position_y = liste_cartes[perso.carte].tp[i][2][1]
+                            perso.carte = liste_cartes[perso.carte].tp[i][0]
+                            
             else:
                 self.orientation = self.perso_h
 
@@ -131,12 +174,20 @@ class Joueur:
             if self.perso_g == self.orientation:
                 self.orientation = self.perso_g
                 if (self.position_x-30, self.position_y) not in liste_cartes[perso.carte].collisions:
+                
                     if self.position_x > 100:
                         self.position_x -= 30
                     else:
                         liste_cartes[int(liste_cartes[perso.carte].directions["gauche"])].afficher_carte(fenetre)
                         perso.carte = int(liste_cartes[perso.carte].directions["gauche"])
                         perso.position_x = 100+600-30
+                        
+                    for i in range(len(liste_cartes[perso.carte].tp)):
+                        if liste_cartes[perso.carte].tp[i][1] == (self.position_x, self.position_y):
+                            self.position_x = liste_cartes[perso.carte].tp[i][2][0]
+                            self.position_y = liste_cartes[perso.carte].tp[i][2][1]
+                            perso.carte = liste_cartes[perso.carte].tp[i][0] 
+                            
             else:
                 self.orientation = self.perso_g
                     
@@ -144,14 +195,26 @@ class Joueur:
             if self.perso_d == self.orientation:
                 self.orientation = self.perso_d
                 if (self.position_x+30, self.position_y) not in liste_cartes[perso.carte].collisions:
+                
+                    # print((self.position_x + 30, self.position_y))
+                    # print(liste_cartes[perso.carte].tp)
+                    
                     if self.position_x < 650:
                         self.position_x += 30
                     else:
                         liste_cartes[int(liste_cartes[perso.carte].directions["droite"])].afficher_carte(fenetre)
                         perso.carte = int(liste_cartes[perso.carte].directions["droite"])
                         perso.position_x = 100
+                        
+                    for i in range(len(liste_cartes[perso.carte].tp)):
+                        if liste_cartes[perso.carte].tp[i][1] == (self.position_x, self.position_y):
+                            self.position_x = liste_cartes[perso.carte].tp[i][2][0]
+                            self.position_y = liste_cartes[perso.carte].tp[i][2][1]
+                            perso.carte = liste_cartes[perso.carte].tp[i][0]
+                            
             else:
                 self.orientation = self.perso_d
+                
         # print(liste_cartes[perso.carte].collisions)
         # print(self.position_x)
         # print(self.position_y)
