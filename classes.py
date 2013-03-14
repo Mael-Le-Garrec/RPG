947ec835c89fb924388a7583a87f7ed894055f44
Hatrix <hatrix@hatrix.fr>
2013-03-14 20:43:07 +0100

    Correction d'un bug lors de la création de personnage

--- classes.py
+++ classes.py
@@ -1239,7 +1239,7 @@ def afficher_clan(fenetre, actuel):
     fenetre.blit(myfont.render(stats["name"], 1, (0,0,0)), (210, 210))
     # fenetre.blit(myfont.render("Description : " + stats["description"], 1, (0,0,0)), (210, 210+2*20))
     fenetre.blit(myfont.render("Description : H", 1, (0,0,0)), (210, 210+2*20))
-    fenetre.blit(myfont.render("Force : " + stats["Strength"], 1, (0,0,0)), (210, 210+3*20))
+    fenetre.blit(myfont.render("Force : " + stats["strength"], 1, (0,0,0)), (210, 210+3*20))
     fenetre.blit(myfont.render("Agilité : " + stats["agility"], 1, (0,0,0)), (210, 210+4*20))
     fenetre.blit(myfont.render("Chance : " + stats["chance"], 1, (0,0,0)), (210, 210+5*20))
     fenetre.blit(myfont.render("Intelligence : " + stats["intelligence"], 1, (0,0,0)), (210, 210+6*20))
@@ -1270,8 +1270,8 @@ def stats_clan(clan):
             stats["vitality"] = contenu[i].split(":")[1].strip()
         if re.match("^intelligence:", contenu[i]):
             stats["intelligence"] = contenu[i].split(":")[1].strip()
-        if re.match("^Strength:", contenu[i]):
-            stats["Strength"] = contenu[i].split(":")[1].strip()
+        if re.match("^strength:", contenu[i]):
+            stats["strength"] = contenu[i].split(":")[1].strip()
         if re.match("^chance:", contenu[i]):
             stats["chance"] = contenu[i].split(":")[1].strip()
         if re.match("^agility:", contenu[i]):
